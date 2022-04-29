7c44914b0e79f0c309a2a13bc6d3e0be0de9f03d
Mykhailo_Tkachuk <mykhail.tkachuk@gmail.com>
2022-04-29 17:26:51 +0300

    work save #1

--- Image Processing/main.py
+++ Image Processing/main.py
@@ -1,1 +1,21 @@
-import cv2
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+_, im = cv.VideoCapture("ezgif.com-gif-maker.gif").read()
+
+types = ['tiff', 'jpeg', 'png', 'jp2']
+for t in types:
+    cv.imwrite(f'im.{t}',im)
+
+imgs = []
+for t in types:
+    imgs.append(cv.imread(f'im.{t}').astype(np.int32))
+
+res = np.zeros((len(imgs),len(imgs)))
+for i,ii in enumerate(imgs):
+    for k, kk in enumerate(imgs):
+        res[i,k] = np.sum(np.abs(kk-ii))/np.prod(ii.shape)
+
+print(res)
+
